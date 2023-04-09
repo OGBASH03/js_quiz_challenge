d907f8cce9dd435aac4be6c65da021812d45b91c
OGBASH03 <116046076+OGBASH03@users.noreply.github.com>
2023-04-08 20:43:38 -0400

    First Variables Created

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+//VARIABLES WERE CREATED AND USED DOCUEMNT QUERRYSELECOR (ID)
+var head = document.getElementById("head");
+var intro = document.getElementById("introduction");
+var questions = document.getElementById("question");
+var startBtn = document.getElementById("start-quiz");
+var finalScore = document.getElementById("finalscore");
+var userScore = document.getElementById("playerscore")
+var scores = document.getElementById("scores");
+var hiScores = document.getElementById("highscores");
+var time = document.getElementById("time");
+var clock = document.getElementById("clock");
